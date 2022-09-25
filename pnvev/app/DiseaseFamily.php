185b7314ab2b89b0f6e600dabbe4719e715528d8
Carlos Federico <carlos.federico005@gmail.com>
2022-09-25 17:54:30 -0400

    Minor fix (added Disease and DiseaseFamily models). Added Gender, AgeGroup, Disease and DiseaseFamily controllers.

---
+++ pnvev/app/DiseaseFamily.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DiseaseFamily extends Model
+{
+    protected $table = 'pnvev_disease_families';
+}
--- pnvev/app/Http/routes.php
+++ pnvev/app/Http/routes.php
@@ -30,4 +30,13 @@ Route::group(['prefix' => 'api/v1'], function () {
     Route::get('/values/{filterName}', 'Rest\V1\FilterPossibleValuesController@index')->where('filterName', 'GrupoEtareo|Sexo|Year');
     Route::get('tendencies', 'Rest\V1\TendenciesController@index');
     Route::get('horizontalBars', 'Rest\V1\HorizontalBarsController@index');
+    
+    Route::get('/genders', 'Rest\V1\GenderController@index');
+    Route::get('/genders/{id}', 'Rest\V1\GenderController@show');
+    Route::get('/ageGroups', 'Rest\V1\AgeGroupController@index');
+    Route::get('/ageGroups/{id}', 'Rest\V1\AgeGroupController@show');
+    Route::get('/diseaseFamilies', 'Rest\V1\DiseaseFamilyController@index');
+    Route::get('/diseaseFamilies/{id}', 'Rest\V1\DiseaseFamilyController@show');
+    Route::get('/diseases', 'Rest\V1\DiseaseController@index');
+    Route::get('/diseases/{id}', 'Rest\V1\DiseaseController@show');
 });
