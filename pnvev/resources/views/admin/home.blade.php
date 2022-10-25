952c023757d8e786b6fb8763b6971d1bfa950b21
Carlos Federico <carlos.federico005@gmail.com>
2022-10-24 22:47:49 -0300

    Add admin homepage editor (view, controller, rest controller, tinymce).

---
+++ pnvev/resources/views/admin/home.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+</head>
+<body>
+    <header>
+        <div><span>Página de Administracion</span></div>
+        <div>
+            <ul>
+                <a href="{{ route('home') }}">
+                    <li>Volver</li>
+                </a>
+            </ul>
+        </div>
+    </header>
+    <aside>
+        <ul>
+            <a href="{{ route('admin.homePage') }}">
+                <li>Pagina Principal</li>
+            </a>
+            <a href="{{ route('home') }}">
+                <li>Volver</li>
+            </a>
+        </ul>
+        </ul>
+    </aside>
+    <main>
+        <form action="" method="post">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <textarea name="value" id="value"></textarea>
+            <footer>
+                <button type="submit">Guardar</button>
+                <span id="submitStatus">
+                    @if(isset($submitStatus))
+                    Guardado correctamente.
+                    @endif
+                </span>
+            </footer>
+        </form>
+    </main>
+    <script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
+    <script>
+        const REST_HOMEPAGE_CONTENT_URL = "{{ route('rest.homePage') }}";
+        document.addEventListener('DOMContentLoaded', () => {
+        const editor = tinymce.init({
+            selector: 'textarea#value',
+            setup: (editor) => {
+                editor.on('change', () => editor.save());
+                fetch(REST_HOMEPAGE_CONTENT_URL)
+                    .then(res => res.text())
+                    .then(data => {
+                        editor.on('init', () => editor.setContent(data));
+                    });
+            },
+            plugins: [
+                'advlist', 'autolink', 'link', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+                'wordcount', 'visualblocks', 'visualchars', 'fullscreen', 'insertdatetime',
+                'media', 'table', 'template', 'help'
+            ],
+            });
+        });
+    </script>
+</body>
+</html>
