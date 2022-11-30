baa035b9f1916a556c60688814c7c27a07b3b728
Carlos Federico <carlos.federico005@gmail.com>
2022-11-30 18:05:55 -0300

    Mayor fix in leishmaniasis viceral view

--- pnvev/database/migrations/2022_10_21_025611_create_casos_leishmaniasis_viceral.php
+++ pnvev/database/migrations/2022_10_21_025611_create_casos_leishmaniasis_viceral.php
@@ -13,34 +13,31 @@ class CreateCasosLeishmaniasisViceral extends Migration
     public function up()
     {
         DB::statement("CREATE OR REPLACE
-            ALGORITHM = UNDEFINED VIEW `dgvsops`.`v_pnvev_casos_leishmaniasis_visceral` AS
+        ALGORITHM = UNDEFINED VIEW `dgvsops`.`v_pnvev_casos_leishmaniasis_visceral` AS
+        select
+            7 AS `EnfermedadId`,
+            (
             select
-                7 AS `EnfermedadId`,
-                (select name from `dgvsops`.pnvev_disease_v2s where id = 7) AS `TipoEnfermedad`,
-                `ff`.`Sexo` AS `Sexo`,
-                `ff`.`Edad` AS `Edad`,
-                (case
-                    when (`ff`.`GrupoEtareo` = '999 SD') then 'SD'
-                    when (`ff`.`GrupoEtareo` = -(1)) then 'SD'
-                    when isnull(`ff`.`GrupoEtareo`) then 'SD'
-                    else `ff`.`GrupoEtareo`
-                end) AS `GrupoEtareo`,
-                `par`.`name` AS `RegionAdministrativa`,
-                `par`.`id` AS `RegionAdministrativaId`,
-                str_to_date(`ff`.`FechaNotificacion`,
-                '%d/%m/%Y') AS `Fecha`,
-                `ff`.`FechaNotificacionSE` AS `SemanaEpidemiologica`,
-                year(str_to_date(`ff`.`FechaNotificacion`, '%d/%m/%Y')) AS `Year`
+                `dgvsops`.`pnvev_disease_v2s`.`name`
             from
-                (`dgvsops`.`frm_fleishmaniasis` `ff`
-            join `dgvsops`.`pnvev_administrative_regions` `par` on
-                ((convert(`par`.`forms_name`
-                    using utf8mb4) = convert(`ff`.`Departamento_desc`
-                    using utf8mb4))))
+                `dgvsops`.`pnvev_disease_v2s`
             where
-                ((`ff`.`TipoFicha` = 'VICERALH')
-                    and (`ff`.`ClasificacionFinal` = 'CONFIRMADO') 
-                    and (year(str_to_date(`ff`.`FechaNotificacion`, '%d/%m/%Y') >= 2020)));");
+                (`dgvsops`.`pnvev_disease_v2s`.`id` = 7)) AS `TipoEnfermedad`,
+            `ff`.`Sexo` AS `Sexo`,
+            `ff`.`Edad` AS `Edad`,
+            `ff`.`GrupoEtareo` AS `GrupoEtareo`,
+            'DEPRECATED' AS `RegionAdministrativa`,
+            'DEPRECATED' AS `RegionAdministrativaId`,
+            str_to_date(`ff`.`FechaNotificacion`,
+            '%d/%m/%Y') AS `Fecha`,
+            `ff`.`FechaNotificacionSE` AS `SemanaEpidemiologica`,
+            year(str_to_date(`ff`.`FechaNotificacion`, '%d/%m/%Y')) AS `Year`
+        from
+            `dgvsops`.`frm_fleishmaniasis` `ff`
+        where
+            ((`ff`.`TipoFicha` = 'VICERALH')
+                and (`ff`.`ClasificacionFinal` = 'CONFIRMADO')
+                    and (year(str_to_date(`ff`.`FechaNotificacion`, '%d/%m/%Y')) >= 2020));");
     }
 
     /**
