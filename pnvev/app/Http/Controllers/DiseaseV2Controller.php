2d287bd30713814518f9e5a9297085cff7ac8486
Carlos Federico <carlos.federico005@gmail.com>
2022-12-24 00:24:34 -0300

    Add model not found exception handler (redirect to home). Add model not found throw into disease controller.

--- pnvev/app/Http/Controllers/DiseaseV2Controller.php
+++ pnvev/app/Http/Controllers/DiseaseV2Controller.php
@@ -10,37 +10,6 @@ use App\Http\Controllers\Controller;
 class DiseaseV2Controller extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
@@ -48,8 +17,7 @@ class DiseaseV2Controller extends Controller
      */
     public function show($id)
     {
-        $model = \App\DiseaseV2::find($id);
-        error_log($model->children()->get());
+        $model = \App\DiseaseV2::findOrFail($id); // Throws ModelNotFoundException
 
         $current_epiweek = \DB::table('pnvev_epiweek')
             ->select('SemanaEpidemiologica')
@@ -62,59 +30,21 @@ class DiseaseV2Controller extends Controller
         } else {
             $current_epiweek = -1;
         }
+        
+        return view('disease', [
+            'activeDisease' => $model,
 
-        if ($model) {
-            return view('disease', [
-                'activeDisease' => $model,
+            'diseaseFullName' => $model->name,
+            'diseaseId' => $id,
+            'diseaseChildren' => $model->children()->get(),
+            'diseaseCaseDescription' => $model->case_description,
 
-                'diseaseFullName' => $model->name,
-                'diseaseId' => $id,
-                'diseaseChildren' => $model->children()->get(),
-                'diseaseCaseDescription' => $model->case_description,
-
-                'epiweek' => $current_epiweek,
-                'tendenciesTitle' => $model->tendencies_title,
-                'childrenTendenciesTitle' => $model->children_tendencies_title,
-                'distributionTitle' => $model->distribution_title,
-                'regionsHeatmapTitle' => $model->regions_heatmap_title,
-                'districtsHeatmapTitle' => $model->districts_heatmap_title,
-            ]);
-        } else {
-            abort(404); // TODO Remove this line and replace with a 404 page
-        }
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+            'epiweek' => $current_epiweek,
+            'tendenciesTitle' => $model->tendencies_title,
+            'childrenTendenciesTitle' => $model->children_tendencies_title,
+            'distributionTitle' => $model->distribution_title,
+            'regionsHeatmapTitle' => $model->regions_heatmap_title,
+            'districtsHeatmapTitle' => $model->districts_heatmap_title,
+        ]);
     }
 }
