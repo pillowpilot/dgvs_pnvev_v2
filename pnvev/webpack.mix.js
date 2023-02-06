b7c27be7a4383093f141b7d0afad3810bfb14c48
Carlos Federico <carlos.federico005@gmail.com>
2023-02-05 23:35:34 -0300

    Add webpack configuration

--- pnvev/webpack.mix.js
+++ pnvev/webpack.mix.js
@@ -1,8 +1,30 @@
 const mix = require('laravel-mix');
 
+/*
+ * See https://stackoverflow.com/a/71116953 about webpackConfig details.
+ */
+
 mix.sourceMaps(false, 'source-map') // Generate .css.map files (even in devlopment)
+    .webpackConfig({
+        module: {
+            rules: [
+              {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+    })
     .sass('resources/assets/sass/admin/admin.scss', 'public/css_v2')
     .sass('resources/assets/sass/admin/user.scss', 'public/css_v2')
-    .sass('resources/assets/sass/admin/home.scss', 'public/css_v2')
     .js('resources/assets/js/admin.js', 'public/js_v2')
+    .js('resources/assets/js/dbviews.js', 'public/js_v2')
     .js('resources/assets/js/diseases.js', 'public/js_v2');
