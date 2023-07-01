b037be349dae77b653ecda120121263d28300f8d
Carlos Federico <carlos.federico005@gmail.com>
2023-07-01 03:15:40 -0400

    fix (model case): Update table name (pnvev_casos)

--- pnvev/app/Caso.php
+++ pnvev/app/Caso.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Caso extends Model
 {
-    protected $table = 'v_pnvev_casos';
+    protected $table = 'pnvev_casos';
 
     protected $fillable = ['TipoEnfermedad', 'TipoCaso', 'ClasificacionFinal', 'Sexo', 'Edad', 'GrupoEtareo', 'FechaCarga', 'SemanaEpidemiologica', 'Year'];
 }
